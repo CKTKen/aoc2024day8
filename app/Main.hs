5cdf0c18e7cbc0e35fbb6b809999a19801344efe
BuildTools <unconfigured@null.spigotmc.org>
2024-12-08 14:03:30 -0500

    Played around and got stuck

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,19 @@
 module Main where
 
+import Data.Array
+
+data RectList a = RectList (Int,Int) [[a]] | BadList deriving (Show, Eq)
+
+toRL :: [[a]] -> RectList a
+toRL [[]] = RectList (0,0) []
+toRL xs
+  | rowCheck  = RectList (firstLen, length xs) xs
+  | otherwise = BadList
+  where
+    rowCheck = all ((==) firstLen . length) xs
+    firstLen = length (head xs)
+
+a = listArray (1,2) [3,4]
+
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
